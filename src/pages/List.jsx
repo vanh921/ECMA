c0f0bc853fd9e6985a806bf6a459fc6add6aaca3
Phạm Viêt Anh <vietanhpham9211@gmail.com>
2025-12-05 14:16:04 +0700

    fix:lab5

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -46,22 +46,19 @@ function ListPage() {
             <td className="px-4 py-2 border border-gray-300">{item.name}</td>
             <td className="px-4 py-2 border border-gray-300">{item.price}</td>
             <td className="px-4 py-2 border border-gray-300">
-             <div className="flex gap-2">
-  <button
-    onClick={() => handleDelete(item.id)}
-    className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition"
-  >
-    Delete
-  </button>
+              <div className="flex gap-2">
+            <button
+            onClick={() => handleDelete(item.id)}
+            className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition">Delete
+            </button>
 
-  <Link to={`/edit/${item.id}`}>
-    <button
-      className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-    >
-      Edit
-    </button>
-  </Link>
-</div>
+            <Link to={`/edit/${item.id}`}>
+            <button
+            className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+            Edit
+            </button>
+            </Link>
+              </div>
             </td>
           </tr>
         ))}
